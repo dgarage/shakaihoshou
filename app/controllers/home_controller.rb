48e468d8d09785ce183b3b866c4c260e92d79060
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-05-30 15:03:24 +0900

    Adds header form and index form & enables interaction between forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
 class HomeController < ApplicationController
+	
 
 	def index
-		
+		areas = ['nan', '世田谷区地域社会福祉協議会', '世田谷区', 'test1', 'test2', 'test3']
+		@area_hash = Hash.new
+		areas.each{|area| @area_hash[area] = false}
+
+	end
+
+	def top_search
+		areas = ['nan', '世田谷区地域社会福祉協議会', '世田谷区', 'test1', 'test2', 'test3']
+		@area_hash = Hash.new
+		areas.each{|area| @area_hash[area] = false}
+		@area_hash[params[:selected_area]] = true
+		@p = params
+		render 'index'
 	end
 end
