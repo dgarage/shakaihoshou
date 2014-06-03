d7ee32a2bc122bf209cbd99e4f4a41215082ed20
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-06-03 16:48:48 +0900

    Cleans home controller and fixes search_by_area action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,33 +6,17 @@ class HomeController < ApplicationController
 		areas = ['nan', '世田谷区地域社会福祉協議会', '世田谷区', 'test1', 'test2', 'test3', 'test4', 'test5']
 		@area_hash = Hash.new
 		areas.each{|area| @area_hash[area] = false}
-		@cities = ['中野区', '渋谷区', '新宿区', '中野区', '渋谷区', '新宿区', '中野区', '渋谷区', '新宿区', '中野区', '渋谷区', '新宿区']
+		@cities = ['世田谷区', '渋谷区', '新宿区', '中野区', '渋谷区', '新宿区', '中野区', '渋谷区', '新宿区', '中野区', '渋谷区', '新宿区']
 
 	end
 
 	def top_search
-		# areas = ['nan', '世田谷区地域社会福祉協議会', '世田谷区', 'test1', 'test2', 'test3', 'test4', 'test5']
-		# @area_hash = Hash.new
-		# areas.each{|area| @area_hash[area] = false}
-		# @area_hash[params[:selected_area]] = true
-		# render 'refine'
-		p params
-
-		@results = Incident.where({:"0" => params[:area]})
-		# @results = Incident.all
-
-		p @results
-		p 'COOOEEEEEE'
-		# # p @results.first.encoding
-		# @results = Institution.all
-		# @results = Institution.all
+		@results = Incident.where({:"0" => params[:area], :"shin" => params[:shin]})
 		render 'search_results'
 	end
 
 	def search_by_area
-		
-		@results = Institution.all
-		# @results = Institution.where({"都、国、市町村" => params[:area]})
+		@results = Incident.where({:"0" => params[:area]})
 		render 'search_results'
 	end
 end
