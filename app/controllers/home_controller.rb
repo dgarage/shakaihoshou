debdc7e4ffd2116ff210001bb101eed7da614c70
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-06-06 15:15:43 +0900

    Adds another fucking layer to the already overly complicated data structure to divide results into one table for each scene

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -41,41 +41,56 @@ class HomeController < ApplicationController
 			end
 		}
 		
-		# COLLECT ALL QUERIES
-		all_results = Hash.new
-		@cities.each{|city|
-			all_results[city] = Incident.where({:"0" => city})
-		}
-
-		# MAKE SHARED INFO
-		@shared_info = Array.new
-		all_results[@cities[0]].each{|row|
-			shared_info_row = Hash.new
-			shared_info_row["id"] = row[:id]
-			shared_info_row["シーン種別(1)"] = row.attributes["2"]
-			shared_info_row["一般名称（今後増えてきたものにする）"] = row.attributes["5"]
-			if row.attributes["7"]
-				shared_info_row["保障額・内容"] = row.attributes["7"]
-			else
-				shared_info_row["保障額・内容"] = ""
-			end
-			@shared_info.append shared_info_row
-		}
-
-		max_rows = (all_results.values.map{|x| x.length}).max
-		@area_info_by_rows = Array.new(max_rows){Hash.new}
 		
-		all_results.each_entry{|city, list_of_results|
-			list_of_results.each_with_index{ |city_row, index|
-				city_row_hash = Hash.new
-				
-				city_row_hash["女"] = city_row["12"]
-				@area_info_by_rows[index][city] = city_row_hash
+		all_results = Hash.new
+		
+		if params[:scene]
+			@existing_scenes = [params[:scene]]
+			p 'MOTHER FUCKER'
+		else
+			@existing_scenes = (Incident.all.pluck(:"2").uniq[1..-1] + Incident.all.pluck(:"3").uniq[1..-1]).uniq
+			@existing_scenes.delete nil
+		end
+		
+		@area_info_by_scene = Hash[@existing_scenes.map{|k| [k, Array.new]}]
+		@shared_info = Hash[@existing_scenes.map{|k| [k, Array.new]}]
+
+		@existing_scenes.each{|scene|
+			@cities.each{|city|
+				all_results[city] = Incident.where({:"0" => city, :"2" => scene})
+			}
+			
+			# MAKE SHARED INFO
+			all_results[@cities[0]].each{|row|
+				shared_info_row = Hash.new
+				shared_info_row["id"] = row[:id]
+				shared_info_row["シーン種別(1)"] = row.attributes["2"]
+				shared_info_row["一般名称（今後増えてきたものにする）"] = row.attributes["5"]
+				if row.attributes["7"]
+					shared_info_row["保障額・内容"] = row.attributes["7"]
+				else
+					shared_info_row["保障額・内容"] = ""
+				end
+				@shared_info[scene].append shared_info_row
 			}
 
+			max_rows = (all_results.values.map{|x| x.length}).max
+			
+			
+			area_info_by_rows = Array.new(max_rows){Hash.new}
+			
+			all_results.each_entry{|city, list_of_results|
+				list_of_results.each_with_index{ |city_row, index|
+					city_row_hash = Hash.new
+					
+					city_row_hash["女"] = city_row["12"]
+					area_info_by_rows[index][city] = city_row_hash
+				}
 
-		}
-		
+
+			}
+			@area_info_by_scene[scene] = area_info_by_rows	
+		}		
 		render 'search_results'
 	end
 
@@ -84,7 +99,4 @@ class HomeController < ApplicationController
 		render 'incident_details'
 	end
 
-	def search_by_one_area
-
-	end
 end
