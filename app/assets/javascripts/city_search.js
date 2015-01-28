b125f1947b72d5bec8032d41e048e2f83323b89d
Anditto Heristyo <anditto.heristyo@gmail.com>
2015-01-28 17:56:20 +0900

    Fixed area comparison JS

--- app/assets/javascripts/city_search.js
+++ app/assets/javascripts/city_search.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     $('[id=area_checkbox]').click(function(){
       var checked = false;
       $('[id=area_checkbox]').each(function(a, b) {
-        if (($(b).is(':checked'))&&($('[id=area_checkbox]:checked').length < 3)) {
+        if (($(b).is(':checked'))&&($('[id=area_checkbox]:checked').length == 2)) {
           checked = checked | true;
         } else {
           checked = checked | false;
@@ -10,8 +10,10 @@ $( document ).ready(function() {
       });
       if (checked == true){
         $("#btn_area_submit").attr("disabled", false);
+        $("#btn_area_submit").prop("value", "検索");
       } else{
         $("#btn_area_submit").attr("disabled", true);
+        $("#btn_area_submit").prop("value", "2つ選択して下さい");
       }	
     });
 });
