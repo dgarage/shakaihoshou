a09d39dc8d9be842f22a8082e9b37f1c47a053ec
Anditto Heristyo <anditto.heristyo@gmail.com>
2015-05-27 16:33:06 +0900

    Edited output for /datavisual to a reduced view

--- app/controllers/datavisual_controller.rb
+++ app/controllers/datavisual_controller.rb
@@ -37,7 +37,37 @@ class DatavisualController < ApplicationController
     @articles   = get_articles
 
     @column_names = comparison_column_name.map{|x| x[1]}
-    @data = get_column_data
+    @column_names = @column_names - [
+      "通し番号",
+      "賃貸（首都圏賃貸居住用物件の取引動向（2015年01～03月））㎡単価（円）",
+      "賃貸50㎡家賃",
+      "330万円＋家賃",
+      "470万円＋家賃",
+      "640万円＋家賃",
+      "930万円＋家賃",
+      "1,130万円＋家賃",
+      "上限＋家賃",
+      "LR賃貸の50〜60㎡の家賃相場（4人世帯想定）",
+      "330万円＋家賃（園に子ど1人）",
+      "330万円＋家賃（園に子ども２人１歳違いとする）",
+      "470万円＋家賃",
+      "470万円＋家賃（園に子ども２人１歳違いとする）",
+      "640万円＋家賃",
+      "640万円＋家賃（園に子ども２人１歳違いとする）",
+      "930万円＋家賃",
+      "930万円＋家賃（園に子ども２人１歳違いとする）",
+      "1,130万円＋家賃",
+      "1,130万円＋家（園に子ども２人１歳違いとする）",
+      "上限＋家賃",
+      "上限＋家賃（園に子ども２人１歳違いとする）"
+    ] 
+    @data = Array.new
+    raw_data = get_column_data
+    raw_data.each do |row|
+      row.delete_at(0)
+      21.times { row.delete_at(-2) }
+      @data << row
+    end
 
     render 'datavisual/test'
   end
